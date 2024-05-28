386f7a4af69df02860539769c68b2b540785b7e2
yusuf kina <yusufkina61@gmail.com>
2024-05-28 15:26:40 +0300

    mobil de menü açıkken zoom özelliği kapatıldı

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const menu = document.querySelector(".menu");
-const viewportMetaTag = document.querySelector("meta[name=viewport]");
+const viewportMetaTag = document.querySelector("meta[name=viewport]") || document.createElement('meta');
+
+if (!viewportMetaTag.parentNode) {
+    viewportMetaTag.name = "viewport";
+    document.head.appendChild(viewportMetaTag);
+}
 
 open.addEventListener("click", () => {
     menu.classList.add("show-nav");
     window.scrollTo(0, 0); 
-    document.body.style.overflow = 'hidden';
-    if (viewportMetaTag) {
-        viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
-    }
+    document.body.style.overflow = 'hidden'; 
+    viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
 });
 
 close.addEventListener("click", () => {
     menu.classList.remove("show-nav");
-    document.body.style.overflow = '';
-    if (viewportMetaTag) {
-        viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1.0");
-    }
+    document.body.style.overflow = ''; 
+    viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1.0");
 });
