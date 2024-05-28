d818a52050cd37cc37280dd7706b68304b942710
yusuf kina <yusufkina61@gmail.com>
2024-05-28 17:21:08 +0300

    mobil düzenlemeleri

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const menu = document.querySelector(".menu");
-const viewportMetaTag = document.querySelector("meta[name=viewport]") || document.createElement('meta');
 
-if (!viewportMetaTag.parentNode) {
+let viewportMetaTag = document.querySelector("meta[name=viewport]");
+
+if (!viewportMetaTag) {
+    viewportMetaTag = document.createElement('meta');
     viewportMetaTag.name = "viewport";
     document.head.appendChild(viewportMetaTag);
 }
@@ -11,12 +13,16 @@ if (!viewportMetaTag.parentNode) {
 open.addEventListener("click", () => {
     menu.classList.add("show-nav");
     window.scrollTo(0, 0); 
-    document.body.style.overflow = 'hidden'; 
+    document.body.style.overflow = 'hidden';
     viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
+    document.body.style.position = 'fixed';
+    document.body.style.width = '100%';
 });
 
 close.addEventListener("click", () => {
     menu.classList.remove("show-nav");
     document.body.style.overflow = ''; 
     viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1.0");
+    document.body.style.position = '';
+    document.body.style.width = '';
 });
