4311999348fec7b475a77be25fd44150c89deb0a
yusuf kina <yusufkina61@gmail.com>
2024-05-28 15:21:22 +0300

    mobil de menü açıkken zoom özelliği kapatıldı

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const menu = document.querySelector(".menu");
+const viewportMetaTag = document.querySelector("meta[name=viewport]");
 
 open.addEventListener("click", () => {
     menu.classList.add("show-nav");
-    window.scrollTo(0, 0); // Sayfanın en üstüne dön
-    document.body.style.overflow = 'hidden'; // Taşmayı gizle
+    window.scrollTo(0, 0); 
+    document.body.style.overflow = 'hidden';
+    if (viewportMetaTag) {
+        viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
+    }
 });
 
 close.addEventListener("click", () => {
     menu.classList.remove("show-nav");
-    document.body.style.overflow = ''; // Taşmayı eski haline getir
+    document.body.style.overflow = '';
+    if (viewportMetaTag) {
+        viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1.0");
+    }
 });
