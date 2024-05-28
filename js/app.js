468cef9151fb7aa87ab748995768800140d3d792
yusuf kina <yusufkina61@gmail.com>
2024-05-28 14:22:02 +0300

    menü kısmı js üzerinden düzenlendi hatalar giderildi

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@ const menu = document.querySelector(".menu");
 
 open.addEventListener("click", () => {
     menu.classList.add("show-nav");
+    window.scrollTo(0, 0); // Scroll to the top
+    document.body.style.overflow = 'hidden'; // Hide overflow
 });
+
 close.addEventListener("click", () => {
     menu.classList.remove("show-nav");
+    document.body.style.overflow = ''; // Revert overflow
 });
-
-
-
